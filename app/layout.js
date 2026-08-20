fa87aaf9ee8d915e8f38a72f297beaf440b60e82
dattd101 <dattd101@gmail.com>
2026-08-20 19:58:34 +0700

    a7
    a7

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
 import './globals.css';
+import Script from 'next/script';
+
+const GA_MEASUREMENT_ID = 'G-L1JNNEFMQC';
 
 export const metadata = {
   title: 'Temp Chat',
@@ -8,7 +11,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="vi">
-      <body suppressHydrationWarning>{children}</body>
+      <body suppressHydrationWarning>
+        {children}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_MEASUREMENT_ID}');
+          `}
+        </Script>
+      </body>
     </html>
   );
 }
